34a655c20a5bc92715fba05794161ec6a0bfba23
ATAR1 <thexenosmilu@gmail.com>
2018-09-11 10:06:40 +0500

    Добавил репозиторий для сохранения интервалов в базу

---
+++ IntervalsDBTypesLibrary/IIntervalsRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace IntervalsDBTypesLibrary
+{
+    public interface IIntervalsRepository
+    {
+        void Save(IEnumerable<Interval> intervals);
+    }
+}
---
+++ IntervalsDBTypesLibrary/IntervalsRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntervalsDBTypesLibrary
+{
+    public class IntervalsRepository : IIntervalsRepository
+    {
+        public void Save(IEnumerable<Interval> intervals)
+        {
+            using (var ctx = new IntervalsDBModelContainer())
+            {
+                ctx.Intervals.AddRange(intervals);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
