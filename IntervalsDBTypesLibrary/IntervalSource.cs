a65f72ce97d30140d4bba8777ef535ebf317920d
ATAR1 <thexenosmilu@gmail.com>
2018-09-11 12:05:54 +0500

    Добавил интерфейс источника данных для диаграммы или хранилища

---
+++ IntervalsDBTypesLibrary/IntervalSource.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using IntervalsDBTypesLibrary;
+
+namespace IntervalsDBTypesLibrary
+{
+    public abstract class IntervalSource
+    {
+        public abstract IEnumerable<Interval> GetData(DateTime beginTime, DateTime endTime);
+    }
+}
