ee262c24d4b202f75ab4f5edff12147e7949ce78
ATAR1 <thexenosmilu@gmail.com>
2018-10-05 09:12:36 +0500

    1

---
+++ IntervalDatasources/ScanerWcfClientDatasource.cs
@@ -0,0 +1,27 @@
+﻿using IntervalsDBTypesLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IntervalDatasources
+{
+    public class ScanerWcfClientDatasource : IntervalSource
+    {
+        private string _forObject;
+
+        public ScanerWcfClientDatasource(string forObject)
+        {
+            _forObject = forObject;
+        }
+
+        public override IEnumerable<Interval> GetData(DateTime beginTime, DateTime endTime)
+        {
+            using (ServiceReference1.DataSourceServiceClient cl = new ServiceReference1.DataSourceServiceClient())
+            {
+                return cl.GetRecordsForPeriod(beginTime, endTime).Select(r => new Interval() { Object = _forObject, Duration = TimeSpan.FromSeconds(r.Duration), StartTime = r.Start, SpecialLevel = r.Status });
+            }
+
+        }
+    }
+}
