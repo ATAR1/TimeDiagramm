4d10c4f2f87f2bb78139b69f0ae73dd3061c18d7
ATAR1 <thexenosmilu@gmail.com>
2018-12-27 16:13:42 +0500

    убрал лишние ответственности

--- TimeDiagrammGeneratorLibrary/GraphicObjects/AxisX.cs
+++ TimeDiagrammGeneratorLibrary/GraphicObjects/AxisX.cs
@@ -1,56 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace TimeDiagrammGeneratorLibrary.GraphicObjects
 {
-    public class AxisX
+    public class AxisX:ChartArea
     {
-        private Chart _chart;
 
-        public AxisX(Chart _chart, int maxValue)
-        {
-            this._chart = _chart;
-            MaxValue = maxValue;
+        public AxisX(Scale scale,ChartArea owner):base(owner)
+        {            
+            Scale = scale;            
         }
-
-        public int Left => _chart.Margin;
-        public int MaxValue { get; private set; }
-        public int Right => _chart.Width - _chart.Margin;
-        public int Bottom => _chart.Height - _chart.Margin;
-        public int PixelsPerDegree => (Right - Left) / MaxValue;
-
-        public IEnumerable<AxisXMark> SplitAxis(int count)
-        {
-            return MarksOfAxis.SplitAxis(this, count);
-        }
-
-        class MarksOfAxis : IEnumerable<AxisXMark>
-        {
-            AxisXMark[] _items;
-
-            private MarksOfAxis() { }
-
-            public static MarksOfAxis SplitAxis(AxisX axis, int count)
-            {
-                MarksOfAxis result = new MarksOfAxis();
-                result._items = new AxisXMark[count];
-                for (int i = 0; i < count; i++)
-                {
-                    result._items[i] = new AxisXMark(axis, i * (axis.MaxValue / count));
-                }
-                return result;
-            }
-
-            public IEnumerator<AxisXMark> GetEnumerator()
-            {
-                return ((IEnumerable<AxisXMark>)_items).GetEnumerator();
-            }
-
-            IEnumerator IEnumerable.GetEnumerator()
-            {
-                return GetEnumerator();
-            }
-        }
-
+                        
+        public IEnumerable<AxisXMark> Marks { get; set; }
+        public IScale Scale { get; }      
+        
     }
 }
