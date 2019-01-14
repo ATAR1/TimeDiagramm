3b9109f96bd9ff3c46145e87f6bdf5d39f1b2a15
ATAR1 <thexenosmilu@gmail.com>
2019-01-14 10:07:40 +0500

    1

---
+++ TimeDiagrammGeneratorLibrary/GraphicObjects/Caption.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace TimeDiagrammGeneratorLibrary.GraphicObjects
+{
+    public class Caption : VisibleChartObject
+    {        
+        public string Text { get; set; }
+        public Font Font { get; set; } = new Font(FontFamily.GenericSansSerif, 10);
+        public int Height => (int)Graphics.FromImage(new Bitmap(1, 1)).MeasureString(Text, Font).Height;
+        public int Left { get; internal set; }
+        public int Bottom { get; internal set; }
+        public int Width => (int)Graphics.FromImage(new Bitmap(1, 1)).MeasureString(Text,Font).Width;               
+        public override void Draw(Graphics gr)
+        {
+            gr.DrawString(Text,Font, Brushes.Black, Left, Bottom-Height);
+        }
+    }
+}
